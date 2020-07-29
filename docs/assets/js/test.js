c922f1aa873a3577c7468108263c952b6d1a4813
Ahmed Bedair <cutie@Ahmeds-MacBook-Pro.local>
2020-07-29 16:32:39 +0200

    Added google sheets api support and test lab-equipment document

--- docs/assets/js/test.js
+++ docs/assets/js/test.js
@@ -1,1 +1,32 @@
-alert("hello");
+var spreadsheetID = "1-ZTCEB_XrMIM65hqTaB8nF_TRgeGFnLu9Ngcj0Y8E8A";
+var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/"+worksheetNumber+"/public/values?alt=json";
+var booking = "https://docs.google.com/spreadsheets/d/1-ZTCEB_XrMIM65hqTaB8nF_TRgeGFnLu9Ngcj0Y8E8A/edit#gid=0&range="
+console.log(url);
+$.getJSON(url, function(data) {
+  var entry = data.feed.entry;
+  console.log(entry);
+  $(entry).each(function() {
+    var category = this.gsx$category.$t;
+    if(category == "-") {
+      return;
+    }
+    var id = category.split(" ").join('_')
+    var device = this.gsx$device.$t;
+    var link = this.gsx$link.$t;
+    var count = this.gsx$count.$t;
+    var status = this.gsx$status.$t;
+    var row = this.gsx$row.$t;
+    console.log(entry);
+    if($("table." + id).length == 0) {
+      $('<h2>'+category+'</h2>').appendTo('#tables');
+      $('<table>').addClass(id).appendTo('#tables');
+
+    }
+    var $tr = $('<tr>').append(
+      $('<td>').html("<a href='"+link+"'>"+device+"</a>"),
+      $('<td>').text("x"+count),
+      $('<td>').text(status),
+      $('<td>').html('<a target="_blank" href="'+booking+row+":"+row+'">reserve</a>')
+    ).appendTo('table.'+id);
+  });
+});
--- docs/pages/lab-equipment/test.md
+++ docs/pages/lab-equipment/test.md
@@ -3,8 +3,11 @@ title: Test
 parent: Lab Equipment
 has_children: false
 nav_order: 5
+worksheet_number: 1
 ---
 
-# Test
+# IoT Devices
 
-<script src="{{ base.url | prepend: site.url }}/assets/js/test.js"></script>
+This is a list of the current IoT devices in the lab. Equipment can be booked from this [google sheets](https://docs.google.com/spreadsheets/d/1-ZTCEB_XrMIM65hqTaB8nF_TRgeGFnLu9Ngcj0Y8E8A/edit?usp=sharing)
+
+{% include load_google_sheet.html worksheet_number=page.worksheet_number %}
